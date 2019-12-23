b064a64a377751123e9d7a7ab21558c957746970
Hector Vasquez <hvasquezdev@gmail.com>
2019-12-22 21:24:56 -0400

    added error-handler middleware

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 
-const { config } = require('./config/index');
+const {
+  config
+} = require('./config/index');
 const moviesApp = require('./routes/movies');
+const {
+  logErrors,
+  errorHandler
+} = require('./utils/middleware/errorHandlers');
 
 app.use(express.json());
 
 moviesApp(app);
 
-app.listen(config.port, function() {
+app.use(logErrors);
+app.use(errorHandler);
+
+app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
 });
