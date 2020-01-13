b9b18c1a90dcaa867fb073ef71a02298caf6eee7
Hector Vasquez <hvasquezdev@gmail.com>
2020-01-12 20:24:11 -0400

    added post method and delete method in userMovies routes

--- routes/userMovies.js
+++ routes/userMovies.js
@@ -5,13 +5,11 @@ const validationHandler = require('../utils/middleware/validationHandler');
 const buildMessage = require('../utils/buildMessage');
 
 const {
-  movieIdSchema
-} = require('../utils/schemas/movies');
-const {
   userIdSchema
 } = require('../utils/schemas/users');
 const {
-  createUserMovieSchema
+  createUserMovieSchema,
+  userMovieIdSchema
 } = require('../utils/schemas/userMovies');
 
 function userMoviesApi(app) {
@@ -36,6 +34,46 @@ function userMoviesApi(app) {
       res.status(200).json({
         data: userMovies,
         message: buildMessage('user movie', 'list')
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/', validationHandler(createUserMovieSchema), async function (req, res, next) {
+    const {
+      body: userMovie
+    } = req;
+
+    try {
+      const createdUserMovieId = await userMoviesService.createUserMovie({
+        userMovie
+      });
+
+      res.status(201).json({
+        data: createdUserMovieId,
+        message: buildMessage('user movie', 'create')
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.delete('/:userMovieId', validationHandler({
+    userMovieId: userMovieIdSchema
+  }, 'params'), async function (req, res, next) {
+    const {
+      userMovieId
+    } = req.params;
+
+    try {
+      const deletedUserMovieId = await userMoviesService.deleteUserMovie({
+        userMovieId
+      });
+
+      res.status(200).json({
+        data: deletedUserMovieId,
+        message: buildMessage('user movie', 'delete')
       });
     } catch (error) {
       next(error);
